6d0a1f689e3be884fd6ada884c1100f83970c0dc
vien huynh <huynhvienha@gmail.com>
2021-06-03 20:51:37 +0700

    vien

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,21 +10,21 @@ class Main extends Component {
         this.state = {
             tenSanPham: '',
             giaCa: '',
-            nguoiChamSoc:'',
-            nguoiBanSp:'',
-            baoQuan:'',
-            noiTrong:'',
-            ngayGieoTrong:'',
-            ngayThuHoach:'',
-            donVi:'',
-            hinhAnh:''
+            nguoiChamSoc: '',
+            nguoiBanSp: '',
+            baoQuan: '',
+            noiTrong: '',
+            ngayGieoTrong: '',
+            ngayThuHoach: '',
+            donVi: '',
+            hinhAnh: ''
         }
     }
-    onChange(event){
+    onChange(event) {
         var target = event.target;
         var name = target.name;
         this.setState({
-            [name] : target.value
+            [name]: target.value
         });
     }
     onSubmit(e) {
@@ -33,13 +33,13 @@ class Main extends Component {
             tenSanPham: this.state.tenSanPham,
             giaCa: this.state.giaCa,
             nguoiChamSoc: this.state.nguoiChamSoc,
-            nguoiBanSp:this.state.nguoiBanSp,
-            baoQuan:this.state.baoQuan,
-            noiTrong:this.state.noiTrong,
-            ngayGieoTrong:this.state.ngayGieoTrong,
-            ngayThuHoach:this.state.ngayThuHoach,
-            donVi:this.state.donVi,
-            hinhAnh:this.state.hinhAnh
+            nguoiBanSp: this.state.nguoiBanSp,
+            baoQuan: this.state.baoQuan,
+            noiTrong: this.state.noiTrong,
+            ngayGieoTrong: this.state.ngayGieoTrong,
+            ngayThuHoach: this.state.ngayThuHoach,
+            donVi: this.state.donVi,
+            hinhAnh: this.state.hinhAnh
         };
         axios.post('http://localhost:4000/products/add', obj)
             .then(res => console.log(res.data));
@@ -51,16 +51,16 @@ class Main extends Component {
             nguoiBanSp: '',
             baoQuan: '',
             noiTrong: '',
-            ngayGieoTrong:'',
-            ngayThuHoach:'',
-            donVi:'',
-            hinhAnh:''
+            ngayGieoTrong: '',
+            ngayThuHoach: '',
+            donVi: '',
+            hinhAnh: ''
         })
     }
 
     render() {
         return (
-            <div className="col-lg-12" >
+            <div className="col-lg-12" style={{ boxShadow: '5px 10px #888888' }} >
                 <h1 style={{ textAlign: 'center' }} > <span >Thêm Sản Phẩm</span> </h1>
                 <form onSubmit={this.onSubmit} >
                     <div className="row">
@@ -69,14 +69,14 @@ class Main extends Component {
                                 className="form-control"
                                 name="nguoiChamSoc"
                                 placeholder="Người Chăm Sóc"
-                                onChange = {this.onChange}
+                                onChange={this.onChange}
                                 value={this.state.nguoiChamSoc}
-                                />
+                            />
                         </div>
                         <div className="form-group col-4" >
                             <input id="nguoiBanSp" type="text"
                                 name="nguoiBanSp"
-                                onChange={this.onChange} 
+                                onChange={this.onChange}
                                 value={this.state.nguoiBanSp}
                                 className="form-control "
                                 placeholder="Người Bán Sản Phẩm"
@@ -86,7 +86,7 @@ class Main extends Component {
                             <input type="text"
                                 name="baoQuan"
                                 value={this.state.baoQuan}
-                                onChange={this.onChange} 
+                                onChange={this.onChange}
                                 className=" form-control"
                                 placeholder="Bảo Quản"
                                 required />
@@ -99,7 +99,7 @@ class Main extends Component {
                                 ref={
                                     (input) => { this.productName = input }
                                 }
-                                onChange={this.onChange} 
+                                onChange={this.onChange}
                                 value={this.state.tenSanPham}
                                 className=" form-control"
                                 placeholder="Tên Sản Phẩm"
@@ -111,7 +111,7 @@ class Main extends Component {
                                 ref={
                                     (input) => { this.productPrice = input }
                                 }
-                                onChange={this.onChange} 
+                                onChange={this.onChange}
                                 value={this.state.giaCa}
                                 className="form-control "
                                 name="giaCa"
@@ -120,7 +120,7 @@ class Main extends Component {
                         </div>
                         <div className="form-group col-4" >
                             <input type="text"
-                                onChange={this.onChange} 
+                                onChange={this.onChange}
                                 value={this.state.noiTrong}
                                 className=" form-control"
                                 placeholder="Nơi Trồng"
@@ -132,7 +132,7 @@ class Main extends Component {
                     <div className="row">
                         <div className="form-group col-4" >
                             <input id="ngayGieoTrong" type="datetime-local"
-                                onChange={this.onChange} 
+                                onChange={this.onChange}
                                 value={this.state.ngayGieoTrong}
                                 className=" form-control"
                                 placeholder="Ngày gieo trồng"
@@ -141,7 +141,7 @@ class Main extends Component {
                         </div>
                         <div className="form-group col-4" >
                             <input id="ngayThuHoach" type="datetime-local"
-                                onChange={this.onChange} 
+                                onChange={this.onChange}
                                 value={this.state.ngayThuHoach}
                                 className=" form-control"
                                 placeholder="Ngày Thu Hoạch"
@@ -150,7 +150,7 @@ class Main extends Component {
                         </div>
                         <div className="form-group col-4" >
                             <input id="donVi" type="text"
-                                onChange={this.onChange} 
+                                onChange={this.onChange}
                                 value={this.state.donVi}
                                 className="form-control "
                                 placeholder="Đơn vị cung cấp"
@@ -162,7 +162,7 @@ class Main extends Component {
                     <div>
                         <div className=""> <label> Hình ảnh của sản phẩm :</label>
                             <input type="file" id="myFile" name="filename" />
-                            <div style={{ width: '200px', height: '300px', border: 'solid 1px' }}> </div>
+
                         </div>
                     </div>
                     <button type="submit"
